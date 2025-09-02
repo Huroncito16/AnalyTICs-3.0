197796686828753a063b5a7e1bd1ca9d4c5a8a43
Antony <santoslopeztony@gmail.com>
2025-09-01 19:17:29 -0600

    Agregar columna de acciones en la tabla de procesos y mejorar la gestión de eventos para agregar y eliminar procesos

--- js/main.js
+++ js/main.js
@@ -1,37 +1,92 @@
-function addProcess() {
-    const processList = [];
 
-    const processNameInput = document.getElementById('processName');
-    const processTimeInput = document.getElementById('processTime');
-    const processPriorityInput = document.getElementById('processPriority');
-    const addButton = document.getElementById('addButton');
-    const processTableBody = document.querySelector('#processTable tbody');
+const ProcessStore = {
+    processes: [],
 
-    const name = processNameInput.value;
-        const time = processTimeInput.value;
-        const priority = processPriorityInput.value;
+    getProcesses: function() {
+        return this.processes;
+    },
+
+    addProcess: function(process) {
+        this.processes.push(process);
+    },
+
+    deleteProcess: function(index) {
+        if (index >= 0 && index < this.processes.length) {
+            this.processes.splice(index, 1);
+        }
+    }
+};
+
+const UIManager = {
+    elements: {
+        nameInput: document.getElementById('processName'),
+        timeInput: document.getElementById('processTime'),
+        priorityInput: document.getElementById('processPriority'),
+        addButton: document.getElementById('addButton'),
+        tableBody: document.querySelector('#processTable tbody')
+    },
+
+    getInputs: function() {
+        const name = this.elements.nameInput.value;
+        const time = this.elements.timeInput.value;
+        const priority = this.elements.priorityInput.value;
+        return { name, time, priority };
+    },
+
+    clearInputs: function() {
+        this.elements.nameInput.value = '';
+        this.elements.timeInput.value = '';
+        this.elements.priorityInput.value = '';
+    },
+
+    renderTable: function(processes) {
+        this.elements.tableBody.innerHTML = ''; // Limpiar la tabla
+        processes.forEach((process, index) => {
+            const row = this.elements.tableBody.insertRow();
+            row.innerHTML = `
+                <td>${process[0]}</td>
+                <td>${process[1]}</td>
+                <td>${process[2]}</td>
+                <td><button class="delete-btn" data-index="${index}">Eliminar</button></td>
+            `;
+        });
+    }
+};
+
+const App = {
+    init: function() {
+        console.log("Planificador de procesos inicializado.");
+        this.addEventListeners();
+    },
+
+    addEventListeners: function() {
+        UIManager.elements.addButton.addEventListener('click', this.handleAddProcess.bind(this));
+        UIManager.elements.tableBody.addEventListener('click', this.handleDeleteProcess.bind(this));
+    },
+
+    handleAddProcess: function() {
+        const { name, time, priority } = UIManager.getInputs();
 
         if (name && time && priority) {
             const newProcess = [name, parseInt(time), parseInt(priority)];
-            processList.push(newProcess);
-
-            const newRow = document.createElement('tr');
-            newRow.innerHTML = `
-                <td>${name}</td>
-                <td>${time}</td>
-                <td>${priority}</td>
-            `;
-            processTableBody.appendChild(newRow);
-
-            // Limpiar los campos de entrada
-            processNameInput.value = '';
-            processTimeInput.value = '';
-            processPriorityInput.value = '';
-
-            console.log('Procesos actuales:', processList);
+            ProcessStore.addProcess(newProcess);
+            UIManager.renderTable(ProcessStore.getProcesses());
+            UIManager.clearInputs();
+            console.log('Procesos actuales:', ProcessStore.getProcesses());
         } else {
             alert('Por favor, complete todos los campos.');
         }
+    },
+
+    handleDeleteProcess: function(event) {
+        if (event.target.classList.contains('delete-btn')) {
+            const indexToDelete = parseInt(event.target.getAttribute('data-index'));
+            ProcessStore.deleteProcess(indexToDelete);
+            UIManager.renderTable(ProcessStore.getProcesses());
+            console.log('Procesos actuales:', ProcessStore.getProcesses());
+        }
+    }
 };
 
-addButton.addEventListener('click',addProcess);
+// Iniciar la aplicación
+App.init();
